7ad47606610636540b858cde1d985f527920f773
Ademola Jegede <2122998@csl-student.wlv.ac.uk>
2022-03-28 06:07:38 +0100

    added bootstrap

--- app/Views/news/overview.php
+++ app/Views/news/overview.php
@@ -1,16 +1,20 @@
 <h2><?= esc($title) ?></h2>
+<p><a class="btn btn-outline-primary mb-4 mt-4" href="<?=base_url()?>/news/create">create article</a></p>
+
 
 <?php if (! empty($news) && is_array($news)): ?>
 
     <?php foreach ($news as $news_item): ?>
+        <div class="card" style="width: 18rem;">
+        <div class="card-body " >
+        <h3 class="card-title" ><?= esc($news_item['title']) ?></h3>
 
-        <h3><?= esc($news_item['title']) ?></h3>
-
-        <div class="main">
+        <div class="main" class="card-text mb-3">
             <?= esc($news_item['body']) ?>
         </div>
-        <p><a class="btn btn-primary" href="<?=base_url()?>/news/<?= esc($news_item['slug'], 'url') ?>">View article</a></p>
-
+        <p><a class="btn btn-outline-primary mb-4 mt-4" href="<?=base_url()?>/news/<?= esc($news_item['slug'], 'url') ?>">View article</a></p>
+        </div>
+        </div>
     <?php endforeach ?>
 
 <?php else: ?>
