7bc5adac0d6892223b21372338d75d30b358c7d0
Ademola Jegede <2122998@csl-student.wlv.ac.uk>
2022-03-28 05:40:21 +0100

    reconfiguration of CI4

---
+++ app/Controllers/Users.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controllers;
+
+class Users extends BaseController
+{
+    public function login()
+    {
+        print('this is the login ');
+    }
+    public function logout()
+    {
+        print('this is the logout ');
+    }
+}
---
+++ app/Views/templates/header.php
@@ -0,0 +1,9 @@
+<!doctype html>
+<html>
+<head>
+    <title>CodeIgniter Tutorial</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+<body>
+    <div class=".container">
+        <h1><?= esc($title) ?></h1>
